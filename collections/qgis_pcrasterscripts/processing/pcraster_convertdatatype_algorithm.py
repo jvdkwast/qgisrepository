f6c68ce77d62298d68d345e2fa5d11ee9c8269f4
Johannes van der Kwast <jvdkwast@yahoo.com>
2021-05-15 10:26:23 +0200

    Two new tools added. Lookup tool will be further improved.

---
+++ collections/qgis_pcrasterscripts/processing/pcraster_convertdatatype_algorithm.py
@@ -0,0 +1,160 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsDataSourceUri,
+                       QgsProcessingParameterRasterDestination,
+                       QgsProcessingParameterEnum,
+                       QgsProcessingParameterNumber,
+                       QgsProcessingParameterRasterLayer)
+from qgis import processing
+from pcraster import *
+
+
+class PCRasterConvertdatatypeAlgorithm(QgsProcessingAlgorithm):
+    """
+    This is an example algorithm that takes a vector layer and
+    creates a new identical one.
+
+    It is meant to be used as an example of how to create your own
+    algorithms and explain methods and variables used to do it. An
+    algorithm like this will be available in all elements, and there
+    is not need for additional work.
+
+    All Processing algorithms should extend the QgsProcessingAlgorithm
+    class.
+    """
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT_RASTER = 'INPUT'
+    INPUT_DATATYPE = 'INPUT1'
+    OUTPUT_RASTER = 'OUTPUT'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return PCRasterConvertdatatypeAlgorithm()
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'convertdatatype'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr('convert layer data type')
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('PCRaster')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'pcraster'
+
+    def shortHelpString(self):
+        """
+        Returns a localised short helper string for the algorithm. This string
+        should provide a basic description about what the algorithm does and the
+        parameters and outputs associated with it..
+        """
+        return self.tr("Conversion of the layer data type.")
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+        
+        self.addParameter(     
+            QgsProcessingParameterRasterLayer(
+                self.INPUT_RASTER,
+                self.tr('Input raster layer')
+            )
+        )
+        
+        self.datatypes = [self.tr('Boolean'),self.tr('Nominal'),self.tr('Ordinal'),self.tr('Scalar'),self.tr('Directional'),self.tr('LDD')]
+        self.addParameter(
+            QgsProcessingParameterEnum(
+                self.INPUT_DATATYPE,
+                self.tr('Output data type'),
+                self.datatypes,
+                defaultValue=0
+            )
+        )
+        
+
+        self.addParameter(
+            QgsProcessingParameterRasterDestination(
+                self.OUTPUT_RASTER,
+                self.tr("Output raster layer")
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+
+        input_raster = self.parameterAsRasterLayer(parameters, self.INPUT_RASTER, context)
+        output_raster = self.parameterAsRasterLayer(parameters, self.OUTPUT_RASTER, context)
+        #setclone(input_raster.dataProvider().dataSourceUri())
+        input_datatype = self.parameterAsEnum(parameters, self.INPUT_DATATYPE, context)
+        if input_datatype == 0:
+            ConversionResult = boolean(input_raster)
+        elif input_datatype == 1:
+            ConversionResult = nominal(input_raster)
+        elif input_datatype == 2:
+            ConversionResult = ordinal(input_raster)
+        elif input_datatype == 3:
+            ConversionResult = scalar(input_raster)
+        elif input_datatype == 4:
+            ConversionResult = directional(input_raster)
+        else:
+            ConversionResult = ldd(input_raster)
+
+        outputFilePath = self.parameterAsOutputLayer(parameters, self.OUTPUT_RASTER, context)
+
+        report(ConversionResult,outputFilePath)
+
+        results = {}
+        results[self.OUTPUT_RASTER] = output_raster
+        
+        return results
