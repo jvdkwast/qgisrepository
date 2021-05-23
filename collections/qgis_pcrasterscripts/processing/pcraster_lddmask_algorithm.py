482a0a1d4a8ea03ae222d36fa8e0629b66d13846
Johannes van der Kwast <jvdkwast@yahoo.com>
2021-05-23 11:09:56 +0200

    lddmask added

---
+++ collections/qgis_pcrasterscripts/processing/pcraster_lddmask_algorithm.py
@@ -0,0 +1,155 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.core import (QgsProcessing,
+                       QgsProcessingException,
+                       QgsProcessingAlgorithm,
+                       QgsDataSourceUri,
+                       QgsProcessingParameterRasterDestination,
+                       QgsProcessingParameterRasterLayer)
+from qgis import processing
+from pcraster import *
+
+
+class PCRasterLddMaskAlgorithm(QgsProcessingAlgorithm):
+    """
+    This is an example algorithm that takes a vector layer and
+    creates a new identical one.
+
+    It is meant to be used as an example of how to create your own
+    algorithms and explain methods and variables used to do it. An
+    algorithm like this will be available in all elements, and there
+    is not need for additional work.
+
+    All Processing algorithms should extend the QgsProcessingAlgorithm
+    class.
+    """
+
+    # Constants used to refer to parameters and outputs. They will be
+    # used when calling the algorithm from another algorithm, or when
+    # calling from the QGIS console.
+
+    INPUT_LDD = 'INPUT'
+    INPUT_MASK = 'INPUT2'
+    OUTPUT_RASTER = 'OUTPUT'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return PCRasterLddMaskAlgorithm()
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'lddmask'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr('lddmask')
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('PCRaster')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'pcraster'
+
+    def shortHelpString(self):
+        """
+        Returns a localised short helper string for the algorithm. This string
+        should provide a basic description about what the algorithm does and the
+        parameters and outputs associated with it..
+        """
+        return self.tr(
+            """Local drain direction map cut into a (smaller) sound local drain direction map
+            
+            <a href="https://pcraster.geo.uu.nl/pcraster/4.3.1/documentation/pcraster_manual/sphinx/op_lddmask.html">PCRaster documentation</a>
+            
+            Parameters:
+            
+            * <b>Input flow direction raster</b> (required) - Flow direction raster in PCRaster LDD format (see lddcreate)
+            * <b>Input mask raster</b> (required) - Boolean raster
+            * <b>Result lddmask layer</b> (required) - Flow direction raster in PCRaster LDD format for TRUE values in mask raster
+            """
+        )
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+
+        # We add the input DEM.
+        self.addParameter(
+            QgsProcessingParameterRasterLayer(
+                self.INPUT_LDD,
+                self.tr('LDD layer')
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterRasterLayer(
+                self.INPUT_MASK,
+                self.tr('Mask layer')
+            )
+        )
+
+        self.addParameter(
+            QgsProcessingParameterRasterDestination(
+                self.OUTPUT_RASTER,
+                self.tr('Result lddmask layer')
+            )
+        )
+
+    def processAlgorithm(self, parameters, context, feedback):
+        """
+        Here is where the processing itself takes place.
+        """
+
+        input_ldd = self.parameterAsRasterLayer(parameters, self.INPUT_LDD, context)
+        input_mask = self.parameterAsRasterLayer(parameters, self.INPUT_MASK, context)
+        output_raster = self.parameterAsRasterLayer(parameters, self.OUTPUT_RASTER, context)
+        setclone(input_ldd.dataProvider().dataSourceUri())
+        LDD = readmap(input_ldd.dataProvider().dataSourceUri())
+        Mask = readmap(input_mask.dataProvider().dataSourceUri())
+        ResultRaster = lddmask(LDD,Mask)
+        outputFilePath = self.parameterAsOutputLayer(parameters, self.OUTPUT_RASTER, context)
+        report(ResultRaster,outputFilePath)
+
+        results = {}
+        results[self.OUTPUT_RASTER] = outputFilePath
+        
+        return results
