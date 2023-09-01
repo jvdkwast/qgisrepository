d87c5b6d497e0573f3fc34734755d4ed6e79d9be
Hans van der Kwast <jvdkwast@yahoo.com>
2023-09-01 15:36:54 +0200

    combine tool added

---
+++ collections/qgis_pcrasteruserscripts/processing/pcraster_combine.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+"""
+***************************************************************************
+*                                                                         *
+*   This program is free software; you can redistribute it and/or modify  *
+*   it under the terms of the GNU General Public License as published by  *
+*   the Free Software Foundation; either version 2 of the License, or     *
+*   (at your option) any later version.                                   *
+*                                                                         *
+***************************************************************************
+"""
+
+from pcraster import (
+    readmap,
+    setclone,
+    scalar,
+    nominal,
+    report
+)
+
+from qgis import processing
+from qgis.PyQt.QtCore import QCoreApplication
+from qgis.PyQt.QtCore import QCoreApplication, QVariant
+from qgis.core import (
+    QgsProcessing,
+    QgsProcessingAlgorithm,
+    QgsProcessingParameterRasterLayer,
+    QgsProcessingParameterMultipleLayers,
+    QgsProcessingParameterRasterDestination)
+
+#from pcraster_tools.processing.algorithm import PCRasterAlgorithm
+
+class mapaverageAlgorithm(QgsProcessingAlgorithm):
+    INPUT_RASTER = 'INPUT'
+    INPUT_RASTERS = 'INPUT1'
+    OUTPUT_RASTER = 'OUTPUT'
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('Processing', string)
+
+    def createInstance(self):
+        return mapaverageAlgorithm()
+
+    def name(self):
+        """
+        Returns the algorithm name, used for identifying the algorithm. This
+        string should be fixed for the algorithm, and must not be localised.
+        The name should be unique within each provider. Names should contain
+        lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'combine'
+
+    def displayName(self):
+        """
+        Returns the translated algorithm name, which should be used for any
+        user-visible display of the algorithm name.
+        """
+        return self.tr('combine')
+
+    def group(self):
+        """
+        Returns the name of the group this algorithm belongs to. This string
+        should be localised.
+        """
+        return self.tr('PCRaster User Scripts')
+
+    def groupId(self):
+        """
+        Returns the unique ID of the group this algorithm belongs to. This
+        string should be fixed for the algorithm, and must not be localised.
+        The group id should be unique within each provider. Group id should
+        contain lowercase alphanumeric characters only and no spaces or other
+        formatting characters.
+        """
+        return 'pcrasteruser'
+
+    def shortHelpString(self):
+        """
+        Returns a localised short helper string for the algorithm. This string
+        should provide a basic description about what the algorithm does and the
+        parameters and outputs associated with it..
+        """
+        return self.tr(
+            """Calculates average of a raster layer
+          
+          
+            Parameters:
+            
+            * <b>Input raster layers</b> (required) - nominal raster layers to combine
+            * <b>Output raster</b> (required) - nominal raster with result
+            """
+        )
+
+    def initAlgorithm(self, config=None):
+        """
+        Here we define the inputs and output of the algorithm, along
+        with some other properties.
+        """
+
+        self.addParameter(
+            QgsProcessingParameterMultipleLayers(
+                self.INPUT_RASTERS,
+                self.tr('Input Raster Layers'),
+                QgsProcessing.TypeRaster
+            )
+        )
+
+
+        self.addParameter(
+            QgsProcessingParameterRasterDestination(
+                self.OUTPUT_RASTER,
+                self.tr("Output combined raster")
+            )
+        )
+
+    def combine_values(self, arr, index):
+        return arr * (2 ** index)
+    
+    def processAlgorithm(self, parameters, context, feedback):
+        input_rasters = []
+        for layer in self.parameterAsLayerList(parameters, self.INPUT_RASTERS, context):
+            input_rasters.append(layer.source())
+        output_raster = self.parameterAsRasterLayer(parameters, self.OUTPUT_RASTER, context)
+        #setclone(input_rasters[0].dataProvider().dataSourceUri())
+        
+        combined_map = scalar(0)
+        
+        # Iterate through input raster maps
+        for index, input_map in enumerate(input_rasters):
+            input_data = readmap(input_map)
+            contribution = self.combine_values(scalar(input_data), index)
+            combined_map = scalar(combined_map) + scalar(contribution)
+            combined_map = nominal(combined_map)
+        
+        outputFilePath = self.parameterAsOutputLayer(parameters, self.OUTPUT_RASTER, context)
+
+        report(combined_map,outputFilePath)
+        
+#        self.set_output_crs(output_file=outputFilePath, crs=input_raster.crs(), feedback=feedback, context=context)
+
+        return {self.OUTPUT_RASTER: outputFilePath}
